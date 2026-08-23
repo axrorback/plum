414fb5d75b48b16f063614d1e7378470f7367b8e
axrorback <axrorback@gmail.com>
2026-08-23 14:31:32 +0500

    Add serializers for webhook handling and order creation in Plum app

---
+++ plum/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+class PlumWebhookSerializer(serializers.Serializer):
+    method = serializers.ChoiceField(choices=("check", "perform"))
+
+
+class PlumCheckSerializer(serializers.Serializer):
+    amount = serializers.IntegerField(min_value=1,)
+    fields = serializers.DictField(child=serializers.CharField(allow_blank=True,))
+
+
+class PlumPerformSerializer(serializers.Serializer):
+    transactionId = serializers.CharField(max_length=255,)
+    id = serializers.UUIDField()
+    date = serializers.DateTimeField()
+    cardNumber = serializers.CharField(max_length=32,allow_blank=True,required=False,)
+
+
+class CreateOrderSerializer(serializers.Serializer):
+    account = serializers.CharField(max_length=255,)
+    amount = serializers.IntegerField(min_value=1,)
+    purpose = serializers.CharField(max_length=500)
+    return_url = serializers.URLField(required=False,allow_blank=True,)
