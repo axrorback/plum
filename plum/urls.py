091540a8c27e210edc12f3250f3445bc07d33ab1
axrorback <axrorback@gmail.com>
2026-08-28 10:13:45 +0500

    Refactor URLs: switch from `path` to `re_path` for webhook endpoints

--- plum/urls.py
+++ plum/urls.py
@@ -1,9 +1,23 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from .views import CreateOrderAPIView , PlumCheckWebhookAPIView , PlumPerformWebhookAPIView
+from .views import (
+    CreateOrderAPIView,
+    PlumCheckWebhookAPIView,
+    PlumPerformWebhookAPIView,
+)
 
 urlpatterns = [
-    path("order/",CreateOrderAPIView.as_view(),name="create-order"),
-    path("webhook/check/",PlumCheckWebhookAPIView.as_view(),name="webhook_check"),
-    path("webhook/perform/",PlumPerformWebhookAPIView.as_view(),name="webhook_perform"),
+    path("order/", CreateOrderAPIView.as_view(), name="create-order"),
+
+    re_path(
+        r"^webhook/check/?$",
+        PlumCheckWebhookAPIView.as_view(),
+        name="webhook_check",
+    ),
+
+    re_path(
+        r"^webhook/perform/?$",
+        PlumPerformWebhookAPIView.as_view(),
+        name="webhook_perform",
+    ),
 ]
