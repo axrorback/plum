1129bd7586b8305f5924a32f4cdfbd725a2c4754
axrorback <axrorback@gmail.com>
2026-08-28 06:51:19 +0500

    Split Plum webhook URL into separate endpoints for "check" and "perform" actions

--- plum/urls.py
+++ plum/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CreateOrderAPIView , PlumWebhookAPIView
+from .views import CreateOrderAPIView , PlumCheckWebhookAPIView , PlumPerformWebhookAPIView
 
 urlpatterns = [
     path("order/",CreateOrderAPIView.as_view(),name="create-order"),
-    path("webhook/",PlumWebhookAPIView.as_view(),name="webhook"),
+    path("webhook/check/",PlumCheckWebhookAPIView.as_view(),name="webhook_check"),
+    path("webhook/perform/",PlumPerformWebhookAPIView.as_view(),name="webhook_perform"),
 ]
